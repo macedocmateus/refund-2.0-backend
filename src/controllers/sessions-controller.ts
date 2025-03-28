ae3309e79ab7afe648c79917bdb76806461019a8
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-28 16:17:46 -0300

    validating user email

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
 import { z } from "zod";
+import { compare } from "bcrypt";
 
 class SessionsController {
     async create(request: Request, response: Response) {
@@ -9,6 +12,17 @@ class SessionsController {
         });
 
         const { email, password } = bodySchema.parse(request.body);
+
+        const user = await prisma.user.findFirst({ where: { email } });
+
+        if (!user) {
+            throw new AppError("Email ou senha inválido", 401);
+        }
+
+        const passwordMatched = await compare(password, user.password);
+        if (!passwordMatched) {
+            throw new AppError("Email ou senha inválido", 401);
+        }
 
         response.json({ email, password });
     }
