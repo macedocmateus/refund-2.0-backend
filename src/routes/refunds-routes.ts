2af4f41291d347a8f727c6bbca97da40b4ac9e4f
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-28 16:59:16 -0300

    init refunds

---
+++ src/routes/refunds-routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+import { RefundsController } from "@/controllers/refunds-controller";
+
+const refundsRoutes = Router();
+const refundsController = new RefundsController();
+
+refundsRoutes.post("/", refundsController.create);
+
+export { refundsRoutes };
