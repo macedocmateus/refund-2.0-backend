0f9a6ea6c5c7bb927e784987a35887bff34c62e3
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-30 20:26:43 -0300

    validating refunds data

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,7 +1,32 @@
 import { Request, Response } from "express";
+import { z } from "zod";
+
+const CategoriesEnum = z.enum([
+    "food",
+    "others",
+    "services",
+    "transport",
+    "accommodation",
+]);
 
 class RefundsController {
     async create(request: Request, response: Response) {
+        const bodySchema = z.object({
+            name: z
+                .string()
+                .trim()
+                .min(1, { message: "Informe o nome da solicitação" }),
+            category: CategoriesEnum,
+            amount: z
+                .number()
+                .positive({ message: "O valor precisa ser positivo" }),
+            filename: z.string().min(20),
+        });
+
+        const { name, category, amount, filename } = bodySchema.parse(
+            request.body
+        );
+
         response.json({ message: "ok" });
     }
 }
