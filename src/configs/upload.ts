72a961ca189c8c62e1c0e86a7f55aaafee4c1f71
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-04-01 16:14:03 -0300

    validating file upload

--- src/configs/upload.ts
+++ src/configs/upload.ts
@@ -5,13 +5,9 @@ import crypto from "node:crypto";
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
 const UPLOADS_FOLDER = path.relative(TMP_FOLDER, "uploads");
 
-const MAX_FILE_SIZE = 1024 * 1024 * 1; // 3MB
-const ACCEPTED_IMAGE_TYPES = [
-    "image/jpeg",
-    "image/jpg",
-    "image/png",
-    "image/svg",
-];
+const MAX_SIZE = 3; // 3MB
+const MAX_FILE_SIZE = 1024 * 1024 * MAX_SIZE;
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
 
 const MULTER = {
     storage: multer.diskStorage({
@@ -30,5 +26,6 @@ export default {
     UPLOADS_FOLDER,
     MULTER,
     MAX_FILE_SIZE,
+    MAX_SIZE,
     ACCEPTED_IMAGE_TYPES,
 };
--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,8 +1,39 @@
 import { Request, Response } from "express";
+import z from "zod";
+
+import uploadConfig from "../configs/upload";
 
 class UploadsController {
     async create(request: Request, response: Response) {
-        response.json({ message: "ok" });
+        try {
+            const fileSchema = z
+                .object({
+                    filename: z.string().min(1, "Arquivo obrigatório"),
+                    mimetype: z
+                        .string()
+                        .refine(
+                            (type) =>
+                                uploadConfig.ACCEPTED_IMAGE_TYPES.includes(
+                                    type
+                                ),
+                            `Formato de arquivo inválido. Formatos permitidos: ${uploadConfig.ACCEPTED_IMAGE_TYPES} `
+                        ),
+                    size: z
+                        .number()
+                        .positive()
+                        .refine(
+                            (size) => size <= uploadConfig.MAX_FILE_SIZE,
+                            `Arquivo excede o tamanho máximo de ${uploadConfig.MAX_SIZE}MB`
+                        ),
+                })
+                .passthrough();
+
+            const { file } = fileSchema.parse(request.file);
+
+            response.json({ message: "ok" });
+        } catch (error) {
+            throw error;
+        }
     }
 }
 
